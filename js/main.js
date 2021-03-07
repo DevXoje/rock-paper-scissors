4018a71253bb1a04cf2287b23a749b53d5f1afe8
XojeHome <dawxoje@gmail.com>
2021-03-07 08:50:55 +0100

    Combate: Logica empezada, voy a empezar estilo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 "use strict";
+var mainPlayer;
+var combate;
+//Player
+var Player = /** @class */ (function () {
+    function Player(arma) {
+        if (arma === void 0) { arma = ""; }
+        this.arma = new Arma(arma);
+    }
+    return Player;
+}());
+//Arma
 var Arma = /** @class */ (function () {
     function Arma(type) {
         this.type = type;
-        this.container = "\n\t\t\t<figure class=\"arma " + this.type + "\">\n                <img src=\"./images/icon-" + this.type + ".svg\" alt=\"logo_game\" height=\"50px\" width=\"50px\" />\n            </figure>";
+        this.container = "\n\t\t\t<figure class=\"arma " + this.type + "\" onclick=\"Arma.selected(event)\">\n                <img src=\"./images/icon-" + this.type + ".svg\" alt=\"logo_game\" height=\"50px\" width=\"50px\" />\n            </figure>";
     }
     Arma.getAllTypes = function () {
         var output = document.createElement("div");
@@ -13,11 +24,52 @@ var Arma = /** @class */ (function () {
         }
         return output.innerHTML;
     };
+    Arma.getRandomType = function () {
+        var rng = Math.floor(Math.random() * Arma.types.length);
+        var output = new Arma(Arma.types[rng]);
+        console.log(rng);
+        console.log(output);
+        return output.container;
+    };
+    Arma.selected = function (e) {
+        var padre = e.target;
+        if (!padre.querySelector("img")) {
+            padre = padre.parentElement;
+        }
+        for (var i = 0; i < this.types.length; i++) {
+            var type = this.types[i];
+            for (var j = 0; j < padre.classList.length; j++) {
+                var styleClass = padre.classList[j];
+                if (styleClass == type) {
+                    mainPlayer = new Player(type);
+                    configCombate();
+                }
+            }
+        }
+    };
     Arma.types = ["paper", "scissors", "rock"];
     return Arma;
 }());
+//Combate
+var Combate = /** @class */ (function () {
+    //rival: Arma;
+    function Combate(player) {
+        this.player = player;
+        this.playerWrapper = document.querySelector(".player");
+        this.rivalWrapper = document.querySelector(".rival");
+        this.playerWrapper.innerHTML = this.player.arma.container;
+    }
+    return Combate;
+}());
+var chosingWrapper = document.querySelector(".chosing");
+var combateWrapper = document.querySelector(".combate");
 function configChosing() {
-    var wrapper = document.querySelector(".chosing");
-    wrapper.innerHTML = Arma.getAllTypes();
+    chosingWrapper.innerHTML = Arma.getAllTypes();
+}
+function configCombate() {
+    chosingWrapper.style.display = "none";
+    combate = new Combate(mainPlayer);
+    combate.playerWrapper.innerHTML = mainPlayer.arma.container;
+    combate.rivalWrapper.innerHTML = Arma.getRandomType();
 }
 configChosing();
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,13 @@
+let mainPlayer: Player;
+let combate: Combate;
+//Player
+class Player {
+	arma: Arma;
+	constructor(arma = "") {
+		this.arma = new Arma(arma);
+	}
+}
+//Arma
 class Arma {
 	static types = ["paper", "scissors", "rock"];
 	type: string;
@@ -5,7 +15,7 @@ class Arma {
 	constructor(type: string) {
 		this.type = type;
 		this.container = `
-			<figure class="arma ${this.type}">
+			<figure class="arma ${this.type}" onclick="Arma.selected(event)">
                 <img src="./images/icon-${this.type}.svg" alt="logo_game" height="50px" width="50px" />
             </figure>`;
 	}
@@ -19,10 +29,57 @@ class Arma {
 		return output.innerHTML;
 
 	}
-}
+	static getRandomType(): string {
+		const rng = Math.floor(Math.random() * Arma.types.length);
 
+		const output: Arma = new Arma(Arma.types[rng]);
+
+		console.log(rng);
+		console.log(output);
+		return output.container;
+	}
+	static selected(e: Event) {
+		let padre: HTMLElement = e.target as HTMLElement;
+		if (!padre.querySelector("img")) {
+			padre = padre.parentElement as HTMLElement;
+		}
+		for (let i = 0; i < this.types.length; i++) {
+			const type = this.types[i];
+			for (let j = 0; j < padre.classList.length; j++) {
+				const styleClass = padre.classList[j];
+				if (styleClass == type) {
+					mainPlayer = new Player(type);
+					configCombate();
+
+				}
+			}
+		}
+	}
+}
+//Combate
+class Combate {
+	playerWrapper: HTMLDivElement;
+	rivalWrapper: HTMLDivElement;
+	player: Player;
+	//rival: Arma;
+	constructor(player: Player) {
+		this.player = player;
+		this.playerWrapper = document.querySelector(".player") as HTMLDivElement;
+		this.rivalWrapper = document.querySelector(".rival") as HTMLDivElement;
+		this.playerWrapper.innerHTML = this.player.arma.container;
+
+	}
+}
+const chosingWrapper = document.querySelector(".chosing") as HTMLDivElement;
+const combateWrapper = document.querySelector(".combate") as HTMLDivElement;
 function configChosing() {
-	const wrapper = document.querySelector(".chosing") as HTMLDivElement;
-	wrapper.innerHTML = Arma.getAllTypes();
+	chosingWrapper.innerHTML = Arma.getAllTypes();
+}
+function configCombate() {
+	chosingWrapper.style.display = "none";
+	combate = new Combate(mainPlayer);
+	combate.playerWrapper.innerHTML = mainPlayer.arma.container;
+	combate.rivalWrapper.innerHTML = Arma.getRandomType();
+
 }
 configChosing();

a7229991ef374c51805e614cc8e49a8140cacb46
XojeHome <dawxoje@gmail.com>
2021-03-08 08:49:34 +0100

    Layout de chosing en mobile apanyao

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var mainPlayer;
 var combate;
 //Player
 var Player = /** @class */ (function () {
     function Player(arma) {
         if (arma === void 0) { arma = ""; }
-        this.arma = new Arma(arma);
+        switch (arma) {
+            case "paper":
+                this.arma = new Paper();
+                break;
+            case "scissors":
+                this.arma = new Scissors();
+                break;
+            case "rock":
+                this.arma = new Rock();
+                break;
+            default:
+                this.arma = new Arma(arma);
+                break;
+        }
+        this.arma.container = this.arma.container.replace("Arma.selected(event)", "");
     }
     return Player;
 }());
 //Arma
 var Arma = /** @class */ (function () {
     function Arma(type) {
+        this.beats = "";
         this.type = type;
         this.container = "\n\t\t\t<figure class=\"arma " + this.type + "\" onclick=\"Arma.selected(event)\">\n                <img src=\"./images/icon-" + this.type + ".svg\" alt=\"logo_game\" height=\"50px\" width=\"50px\" />\n            </figure>";
     }
@@ -26,10 +54,8 @@ var Arma = /** @class */ (function () {
     };
     Arma.getRandomType = function () {
         var rng = Math.floor(Math.random() * Arma.types.length);
-        var output = new Arma(Arma.types[rng]);
-        console.log(rng);
-        console.log(output);
-        return output.container;
+        var output = new Player(Arma.types[rng]);
+        return output;
     };
     Arma.selected = function (e) {
         var padre = e.target;
@@ -50,26 +76,97 @@ var Arma = /** @class */ (function () {
     Arma.types = ["paper", "scissors", "rock"];
     return Arma;
 }());
+var Rock = /** @class */ (function (_super) {
+    __extends(Rock, _super);
+    function Rock() {
+        var _this = _super.call(this, "rock") || this;
+        _this.beats = "scissors";
+        return _this;
+    }
+    return Rock;
+}(Arma));
+var Scissors = /** @class */ (function (_super) {
+    __extends(Scissors, _super);
+    function Scissors() {
+        var _this = _super.call(this, "scissors") || this;
+        _this.beats = "paper";
+        return _this;
+    }
+    return Scissors;
+}(Arma));
+var Paper = /** @class */ (function (_super) {
+    __extends(Paper, _super);
+    function Paper() {
+        var _this = _super.call(this, "paper") || this;
+        _this.beats = "rock";
+        return _this;
+    }
+    return Paper;
+}(Arma));
 //Combate
 var Combate = /** @class */ (function () {
-    //rival: Arma;
     function Combate(player) {
         this.player = player;
+        this.rival = Arma.getRandomType();
         this.playerWrapper = document.querySelector(".player");
         this.rivalWrapper = document.querySelector(".rival");
         this.playerWrapper.innerHTML = this.player.arma.container;
     }
+    Combate.prototype.fight = function () {
+        var output;
+        if (this.player.arma.beats == this.rival.arma.type) {
+            output = true;
+        }
+        else if (this.rival.arma.beats == this.player.arma.type) {
+            output = false;
+        }
+        return output;
+    };
     return Combate;
 }());
+//Escenas
 var chosingWrapper = document.querySelector(".chosing");
 var combateWrapper = document.querySelector(".combate");
 function configChosing() {
-    chosingWrapper.innerHTML = Arma.getAllTypes();
+    var data = Arma.getAllTypes();
+    var data2 = data.split("<figure");
+    var data3 = new Array();
+    for (var i = 1; i < data2.length; i++) {
+        var dataBruto = data2[i];
+        data3.push("<figure" + dataBruto);
+    }
+    chosingWrapper.innerHTML = "\n\t\t<div class=\"items-top\">" + data3[0] + data3[1] + "</div>\n\t\t<div class=\"item-bottom\">" + data3[2] + "</div>\n\t";
 }
 function configCombate() {
-    chosingWrapper.style.display = "none";
     combate = new Combate(mainPlayer);
-    combate.playerWrapper.innerHTML = mainPlayer.arma.container;
-    combate.rivalWrapper.innerHTML = Arma.getRandomType();
+    var outputRival = combate.rival.arma.container;
+    var resultWrapper = combateWrapper.querySelector(".result");
+    var resultText = resultWrapper.querySelector("h2");
+    var textOutput = "";
+    combateWrapper.style.display = "flex";
+    chosingWrapper.style.display = "none";
+    resultWrapper.style.display = "none";
+    combate.playerWrapper.innerHTML = "\n\t\t\t<h2>YOU PICKED</h2>\n\t\t\t" + mainPlayer.arma.container + "\n\t";
+    combate.rivalWrapper.innerHTML = "\n\t\t<h2>THE HOUSE PICKED</h2>\n\t\t<div class=\"arma\"></div>\n\t";
+    setTimeout(function () {
+        combate.rivalWrapper.innerHTML = "\n\t\t\t<h2>THE HOUSE PICKED</h2>\n\t\t\t" + outputRival + "\n\t";
+        resultWrapper.style.display = "flex";
+        if (combate.fight() == undefined) {
+            textOutput = "EMPATE";
+        }
+        else if (combate.fight()) {
+            textOutput = "WINS";
+        }
+        else {
+            textOutput = "LOSE";
+        }
+        resultText.innerHTML = "YOU " + textOutput;
+    }, 2000);
 }
 configChosing();
+//Popup Rules
+var btn_popupRules = document.getElementById("btn_showRules");
+var rulesWrapper = document.getElementById("rules");
+btn_popupRules.onclick = function () {
+    rulesWrapper.style.display = "block";
+};
--- ts/main.ts
+++ ts/main.ts
@@ -3,8 +3,24 @@ let combate: Combate;
 //Player
 class Player {
 	arma: Arma;
+
 	constructor(arma = "") {
-		this.arma = new Arma(arma);
+		switch (arma) {
+			case "paper":
+				this.arma = new Paper();
+				break;
+			case "scissors":
+				this.arma = new Scissors();
+				break;
+			case "rock":
+				this.arma = new Rock();
+				break;
+			default:
+				this.arma = new Arma(arma);
+				break;
+		}
+
+		this.arma.container = this.arma.container.replace("Arma.selected(event)", "");
 	}
 }
 //Arma
@@ -12,7 +28,10 @@ class Arma {
 	static types = ["paper", "scissors", "rock"];
 	type: string;
 	container: string;
+	beats: string;
+
 	constructor(type: string) {
+		this.beats = "";
 		this.type = type;
 		this.container = `
 			<figure class="arma ${this.type}" onclick="Arma.selected(event)">
@@ -29,14 +48,10 @@ class Arma {
 		return output.innerHTML;
 
 	}
-	static getRandomType(): string {
+	static getRandomType(): Player {
 		const rng = Math.floor(Math.random() * Arma.types.length);
-
-		const output: Arma = new Arma(Arma.types[rng]);
-
-		console.log(rng);
-		console.log(output);
-		return output.container;
+		const output: Player = new Player(Arma.types[rng]);
+		return output;
 	}
 	static selected(e: Event) {
 		let padre: HTMLElement = e.target as HTMLElement;
@@ -50,10 +65,27 @@ class Arma {
 				if (styleClass == type) {
 					mainPlayer = new Player(type);
 					configCombate();
-
 				}
 			}
 		}
+	}
+}
+class Rock extends Arma {
+	constructor() {
+		super("rock");
+		this.beats = "scissors";
+	}
+}
+class Scissors extends Arma {
+	constructor() {
+		super("scissors");
+		this.beats = "paper";
+	}
+}
+class Paper extends Arma {
+	constructor() {
+		super("paper");
+		this.beats = "rock";
 	}
 }
 //Combate
@@ -61,25 +93,87 @@ class Combate {
 	playerWrapper: HTMLDivElement;
 	rivalWrapper: HTMLDivElement;
 	player: Player;
-	//rival: Arma;
+	rival: Player;
 	constructor(player: Player) {
 		this.player = player;
+		this.rival = Arma.getRandomType();
 		this.playerWrapper = document.querySelector(".player") as HTMLDivElement;
 		this.rivalWrapper = document.querySelector(".rival") as HTMLDivElement;
 		this.playerWrapper.innerHTML = this.player.arma.container;
 
 	}
+	fight() {
+		let output;
+		if (this.player.arma.beats == this.rival.arma.type) {
+			output = true;
+		}
+		else if (this.rival.arma.beats == this.player.arma.type) {
+			output = false;
+		}
+		return output;
+	}
 }
+//Escenas
 const chosingWrapper = document.querySelector(".chosing") as HTMLDivElement;
 const combateWrapper = document.querySelector(".combate") as HTMLDivElement;
 function configChosing() {
-	chosingWrapper.innerHTML = Arma.getAllTypes();
+	const data = Arma.getAllTypes();
+	const data2 = data.split("<figure");
+	let data3 = new Array();
+	for (let i = 1; i < data2.length; i++) {
+		const dataBruto = data2[i];
+		data3.push("<figure" + dataBruto);
+	}
+
+	chosingWrapper.innerHTML = `
+		<div class="items-top">${data3[0]}${data3[1]}</div>
+		<div class="item-bottom">${data3[2]}</div>
+	`;
 }
 function configCombate() {
-	chosingWrapper.style.display = "none";
 	combate = new Combate(mainPlayer);
-	combate.playerWrapper.innerHTML = mainPlayer.arma.container;
-	combate.rivalWrapper.innerHTML = Arma.getRandomType();
+
+	const outputRival = combate.rival.arma.container;
+	const resultWrapper = combateWrapper.querySelector(".result") as HTMLDivElement;
+	const resultText = resultWrapper.querySelector("h2") as HTMLElement;
+	let textOutput = "";
+	combateWrapper.style.display = "flex";
+	chosingWrapper.style.display = "none";
+	resultWrapper.style.display = "none";
+	combate.playerWrapper.innerHTML = `
+			<h2>YOU PICKED</h2>
+			${mainPlayer.arma.container}
+	`;
+	combate.rivalWrapper.innerHTML = `
+		<h2>THE HOUSE PICKED</h2>
+		<div class="arma"></div>
+	`;
+	setTimeout(() => {
+		combate.rivalWrapper.innerHTML = `
+			<h2>THE HOUSE PICKED</h2>
+			${outputRival}
+	`;
+		resultWrapper.style.display = "flex";
+		if (combate.fight() == undefined) {
+			textOutput = "EMPATE";
+		} else if (combate.fight()) {
+			textOutput = "WINS";
+
+		} else {
+			textOutput = "LOSE";
+		}
+		resultText.innerHTML = `YOU ${textOutput}`;
+	}, 2000);
+
+
+
 
 }
 configChosing();
+//Popup Rules
+const btn_popupRules = document.getElementById("btn_showRules") as HTMLButtonElement;
+const rulesWrapper = document.getElementById("rules") as HTMLDivElement;
+
+btn_popupRules.onclick = () => {
+	rulesWrapper.style.display = "block";
+}
